df6191c132fcb94f661ecca4908a7e9c6ba3bade
ZDEEX <95095652+ZDEEX@users.noreply.github.com>
2025-02-17 00:20:44 +0700

    Update coins_updater.dart
    Signed-off-by: ZDEEX <95095652+ZDEEX@users.noreply.github.com>

--- lib/app_config/coins_updater.dart
+++ lib/app_config/coins_updater.dart
@@ -36,7 +36,7 @@ class CoinUpdater {
   static const coinsRepoBranch = 'master';
 
   static const coinsRepoUrl =
-      'https://raw.githubusercontent.com/komodoce/coins-ce';
+      'https://raw.githubusercontent.com/zdeex/coins-ce';
 
   static const isUpdateEnabled = true;
 
